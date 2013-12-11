132d98188b6db3c5e8be8f0b98b2e7103dbc8749
Kuvaldis <kuvaldis1988@gmail.com>
2013-12-11 16:45:34 +0400

    logback added

--- source/rest-app/src/main/groovy/kuvaldis/makeup/rest/app/Start.groovy
+++ source/rest-app/src/main/groovy/kuvaldis/makeup/rest/app/Start.groovy
@@ -2,6 +2,7 @@ package kuvaldis.makeup.rest.app
 
 import com.google.inject.Guice
 import com.google.inject.Injector
+import groovy.util.logging.Slf4j
 import kuvaldis.makeup.rest.module.AppModule
 import kuvaldis.makeup.rest.server.GrizzlyServer
 import kuvaldis.makeup.shared.config.PropertiesBuilder
@@ -12,13 +13,18 @@ import kuvaldis.makeup.shared.module.SharedModule
  * Date: 10.12.13
  * Time: 14:07
  */
+@Slf4j
 class Start {
     public static void main(String[] args) {
+        def startTime = System.currentTimeMillis()
+        log.info('Start rest application')
         Injector sharedInjector = Guice.createInjector(new SharedModule())
         def appModule = new AppModule(propertiesHolder: sharedInjector.getInstance(PropertiesBuilder).build())
         Injector injector = sharedInjector.createChildInjector(appModule)
         def server = injector.getInstance(GrizzlyServer)
+        log.info('Start http server')
         server.start()
+        log.info("Application started in ${System.currentTimeMillis() - startTime} ms")
         println "Press any key to stop the server..."
         System.in.read()
     }
