38b4bb3f6c0c434dfeae9f9234449e458c0c9a07
Kuvaldis <kuvaldis1988@gmail.com>
2013-12-13 17:01:44 +0400

    password

--- source/lib/src/main/resources/db/migration/V00001__create_user_roles_tables.sql
+++ source/lib/src/main/resources/db/migration/V00001__create_user_roles_tables.sql
@@ -15,6 +15,7 @@ create table if not exists user_auth (
   id int default user_auth_seq.nextval primary key,
   user_id int not null,
   login varchar(255) not null,
+  password varchar(255),
   auth_way varchar(255) not null,
   foreign key (user_id) references user_(id),
   foreign key (auth_way) references auth_way(name)
